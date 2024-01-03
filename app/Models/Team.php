2eb4aa4fc496fcd1eb64f85d83f8bd2db776bdb0
ovobannyovo <ovobannyovo@sun.net.tw>
2024-01-03 13:56:49 +0800

    setting tenant relationships

---
+++ app/Models/Team.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    public function labels():HasMany
+    {
+        return $this->hasMany(Label::class);
+    }
+
+    public function tasks():HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function members():HasMany
+    {
+        return  $this->hasMany(User::class);
+    }
+}
